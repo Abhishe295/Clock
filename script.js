1e6daec7accbd59194e1f0a246c0cbb067c00381
Abhishe295 <abhishekgigaiw@gmail.com>
2024-11-19 13:46:10 +0530

    upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector(".menu-icon");
+let menu = document.querySelector(".menu");
+menuIcon.addEventListener("click",()=>{
+    console.log("clhewguih")
+    menu.classList.toggle("open");
+    console.log(menu.classList)
+})
